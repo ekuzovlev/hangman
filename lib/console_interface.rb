d9d6936ae80255a2050d4e54e2d0577ce3ed18a0
kuz-en <bio19@ya.ru>
2021-07-07 01:07:05 +0300

    paint hangman with colorize gem

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,29 +1,16 @@
 class ConsoleInterface
-  # В константе FIGURES будут лежать все текстовые файлы из папки figures,
-  # помещённые в массив. Один элемент массива — одна строка с содержимым целого
-  # файла.
   FIGURES =
     Dir["#{__dir__}/../data/figures/*.txt"].sort.map { |file_name| File.read(file_name) }
 
-  # На вход конструктор класса ConsoleInterface принимает экземпляр класса Game.
-  #
-  # Экземпляр ConsoleInterface выводит информацию юзеру. При выводе использует
-  # статические строки типа "У вас осталось ошибок:" и информацию из экземпляра
-  # класса Game, дёргая у него методы, которые мы придумали.
   def initialize(game)
     @game = game
   end
 
-  # Выводит в консоль текущее состояние игры, используя данные из экземпляра
-  # класса Game (количество ошибок, сколько осталось попыток и т.д.)
   def print_out
-    puts <<~END
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
-      У вас осталось ошибок: #{@game.errors_allowed}
-
-    END
+    puts "Слово: #{word_to_show}".colorize(:blue)
+    puts "#{figure}".colorize(:yellow)
+    puts "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
+    puts "У вас осталось ошибок: #{@game.errors_allowed}".colorize(:green)
 
     if @game.won?
       puts 'Поздравляем, вы выиграли!'
@@ -39,17 +26,6 @@ class ConsoleInterface
     FIGURES[@game.errors_made]
   end
 
-  # Метод, который готовит слово для вывода "на игровом табло".
-  #
-  # Получает на вход массив: каждый элемент массива соответствует одной букве в
-  # загаданном слове. Но на позиции отгаданной буквы — сама эта буква (строка из
-  # одного символа), а на позиции неотгаданной буквы — nil.
-  #
-  # Метод трансформирует массив (записывает вместо nil два подчеркивания),
-  # и склеивает в строку, разделяя элементы пробелами. Получается как-то так:
-  #
-  # На вход передали: ["К", "О", nil, "О", nil, nil],
-  # на выходе будет: "К О __ О __ __"
   def word_to_show
     result =
       @game.letters_to_guess.map do |letter|
@@ -63,13 +39,10 @@ class ConsoleInterface
     result.join(' ')
   end
 
-  # Получает массив ошибочных букв и склеивает их в строку вида "Х, У"
   def errors_to_show
     @game.errors.join(', ')
   end
 
-  # Получает букву из пользовательского ввода, приводит её к верхнему регистру
-  # и возвращает её
   def get_input
     print 'Введите следующую букву: '
     gets[0].upcase
